8744069099d21a870e08a343131216adab422451
Andrii <A.I.Khmelovsky@gmail.com>
2022-05-02 17:06:37 +0300

    logo and search fix

--- site/components/common/Navbar/Navbar.tsx
+++ site/components/common/Navbar/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar: FC<NavbarProps> = ({ links }) => (
           </nav>
         </div>
         {process.env.COMMERCE_SEARCH_ENABLED && (
-          <div className="justify-center flex-1 hidden lg:flex">
+          <div className="justify-center flex-1 hidden lg:flex h-full">
             <Searchbar />
           </div>
         )}
