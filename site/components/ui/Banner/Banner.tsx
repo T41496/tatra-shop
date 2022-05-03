efb03094500ba009a927e02c76e8874439312653
Andrii <A.I.Khmelovsky@gmail.com>
2022-05-03 11:23:45 +0300

    left bar text

--- site/components/ui/Banner/Banner.tsx
+++ site/components/ui/Banner/Banner.tsx
@@ -7,12 +7,16 @@ export const Banner = () => {
     <div className="grid grid-cols-3 h-[calc(100vh_-_104px)]">
       <div className="bg-[#70877B] flex justify-center">
         <div className="flex flex-col self-center">
-          <h1 className="text-white">tatra hills</h1>
-          <p className="text-white">
+          <h1 className="text-white font-bold text-4xl uppercase">
+            tatra hills
+          </h1>
+          <p className="text-white text-2xl font-normal max-w-[22rem] mt-2">
             Save the planet - give the clothes a second life
           </p>
-          <Link href="/cart">
-            <a>sdf</a>
+          <Link href="/search">
+            <a className="uppercase text-[#70877B] bg-[#FFFFFF] mr-auto mt-8 px-11 py-2 text-2xl">
+              catalog
+            </a>
           </Link>
         </div>
       </div>
