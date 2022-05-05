cadb0af13959b36c3d203a62fb481cd63eaf5247
Andrii <A.I.Khmelovsky@gmail.com>
2022-05-05 17:05:15 +0300

    deleted unused and footer

--- site/components/common/Footer/Footer.tsx
+++ site/components/common/Footer/Footer.tsx
@@ -30,18 +30,74 @@ const Footer: FC<Props> = ({ className, pages }) => {
   return (
     <footer className={rootClassName}>
       <Container>
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 border-b border-accent-2 py-12 text-primary bg-primary transition-colors duration-150 bg-[#161616]">
-          <div className="col-span-1 lg:col-span-2">
+        <div className="grid grid-cols-3 pt-[1.5rem] bg-[#161616]">
+          <div>
             <Link href="/">
-              <a className="flex flex-initial items-center font-bold md:mr-24">
-                <Logo />
-                <span className="text-white">
-                  Save the planet - give clothes a second life
+              <a className="flex flex-initial items-center font-bold md:mr-24 items-end">
+                <Logo className="max-w-[4.5rem]" />
+                <span className="text-white font-normal pl-[2rem]">
+                  Save the planet - give <br /> clothes a second life
                 </span>
               </a>
             </Link>
           </div>
-          <div className="col-span-1 lg:col-span-8">
+          <div className="flex justify-center">
+            <ul>
+              <li className="pb-1">
+                <Link href="/">
+                  <a className="text-white font-normal text-lg">Catalog</a>
+                </Link>
+              </li>
+              <li className="pb-1">
+                <Link href="/">
+                  <a className="text-white font-normal text-lg">Blog</a>
+                </Link>
+              </li>
+              <li className="pb-1">
+                <Link href="/">
+                  <a className="text-white font-normal text-lg">About Us</a>
+                </Link>
+              </li>
+              <li className="pb-1">
+                <Link href="/">
+                  <a className="text-white font-normal text-lg">Contact Us</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="flex justify-center">
+            <ul>
+              <li className="pb-1">
+                <Link href="/">
+                  <a className="text-white font-normal text-lg">
+                    Business terms and conditions
+                  </a>
+                </Link>
+              </li>
+              <li className="pb-1">
+                <Link href="/">
+                  <a className="text-white font-normal text-lg">
+                    Shipping and payment
+                  </a>
+                </Link>
+              </li>
+              <li className="pb-1">
+                <Link href="/">
+                  <a className="text-white font-normal text-lg">
+                    Privacy policy
+                  </a>
+                </Link>
+              </li>
+              <li className="pb-1">
+                <Link href="/">
+                  <a className="text-white font-normal text-lg">
+                    Refund and Returns
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          {/* <div className="col-span-1 lg:col-span-8">
             <div className="grid md:grid-rows-4 md:grid-cols-3 md:grid-flow-col">
               {[...links, ...sitePages].map((page) => (
                 <span key={page.url} className="py-3 md:py-0 md:pb-4">
@@ -53,8 +109,8 @@ const Footer: FC<Props> = ({ className, pages }) => {
                 </span>
               ))}
             </div>
-          </div>
-          <div className="col-span-1 lg:col-span-2 flex items-start lg:justify-end text-primary">
+          </div> */}
+          {/* <div className="col-span-1 lg:col-span-2 flex items-start lg:justify-end text-primary">
             <div className="flex space-x-6 items-center h-10">
               <a
                 className={s.link}
@@ -65,11 +121,20 @@ const Footer: FC<Props> = ({ className, pages }) => {
               </a>
               <I18nWidget />
             </div>
-          </div>
+          </div> */}
         </div>
-        <div className="pt-6 pb-8 flex flex-col md:flex-row justify-between items-center space-y-4 text-accent-6 text-sm">
+        <div className="w-[100%] h-px bg-[#C9C9C9] mt-[2rem] mb-[1.5rem]"></div>
+        <div className="flex items-center justify-center">
+          <a href="#" className="mx-[1rem]">
+            <img src="./instagram-icon.svg" alt="instagram" />
+          </a>
+          <a href="#" className="mx-[1rem]">
+            <img src="./facebook-icon.svg" alt="facebook" />
+          </a>
+        </div>
+        <div className="pt-[1.2rem] pb-[1rem] flex flex-col md:flex-row justify-between items-center space-y-4 text-accent-6 text-sm">
           <div className="m-auto">
-            <span className="font-medium text-white text-base">
+            <span className="font-medium text-white">
               Tatra Hills &copy; {currentYear} All rights reserved.
             </span>
           </div>
