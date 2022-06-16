a3a9db0cf80af0cc06c4e0afe43c90d2c67de848
Sohil Sardhara <sohils@websoptimization.com>
2022-06-16 16:47:23 +0530

    Fixed side cart view for the desktop

--- site/components/common/MobilebarLayout/MobilebarLayout.tsx
+++ site/components/common/MobilebarLayout/MobilebarLayout.tsx
@@ -1,31 +1,40 @@
 import React, { FC } from 'react'
 import { Cross, ChevronLeft } from '@components/icons'
 import { UserNav } from '@components/common'
+import { Logo } from '@components/ui'
 import cn from 'clsx'
-import s from './SidebarLayout.module.css'
+import s from './MobilebarLayout.module.css'
+import { I18nWidget } from '@components/common'
+import Link from 'next/link'
 
 type ComponentProps = { className?: string } & (
   | { handleClose: () => any; handleBack?: never }
   | { handleBack: () => any; handleClose?: never }
 )
 
-const SidebarLayout: FC<ComponentProps> = ({
+const MobilebarLayout: FC<ComponentProps> = ({
   children,
   className,
   handleBack,
   handleClose,
 }) => {
   return (
-    <div className={cn(s.root, className)}>
+    <div className={cn(className, s.root)}>
       <header className={s.header}>
+        <Link href="/">
+          <a className={s.logo} aria-label="Logo">
+            <Logo />
+          </a>
+        </Link>
+        <UserNav className={s.usernav} />
         {handleClose && (
           <button
             onClick={handleClose}
             aria-label="Close"
-            className="hover:text-accent-5 transition ease-in-out duration-150 flex items-center focus:outline-none mr-6"
+            className="hover:text-accent-5 transition ease-in-out duration-150 flex items-center focus:outline-none ml-6"
           >
             <Cross className="h-6 w-6 hover:text-accent-3" />
-            <span className="ml-2 text-accent-7 text-sm ">Close</span>
+            <span className="ml-2 text-accent-7 text-sm hidden">Close</span>
           </button>
         )}
         {handleBack && (
@@ -38,12 +47,15 @@ const SidebarLayout: FC<ComponentProps> = ({
             <span className="ml-2 text-accent-7 text-xs">Back</span>
           </button>
         )}
-
-        <UserNav />
       </header>
-      <div className={s.container}>{children}</div>
+      <div className={s.container}>
+        {children}
+        <div className={cn('block md:hidden', s.mobileLanguageSwitch)}>
+          <I18nWidget />
+        </div>
+      </div>
     </div>
   )
 }
 
-export default SidebarLayout
+export default MobilebarLayout
