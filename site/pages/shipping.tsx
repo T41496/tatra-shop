9b747486c4626cd0d015be55f2cd13156519413d
Andrii <A.I.Khmelovsky@gmail.com>
2022-09-12 13:50:45 +0300

    Shipping text trasnlations

--- site/pages/shipping.tsx
+++ site/pages/shipping.tsx
@@ -14,13 +14,43 @@ export default function Shipping() {
             <p className="text-[30px] text-[#000000] leading-[40px] font-normal">
               {t('title-1')}
             </p>
-          </div>
-        </div>
-        <div className="grid grid-cols-1 gap-x-5 mb-10">
-          <div className="order-last flex flex-col self-center text-xl md:text-[20px]">
+            <br />
             <p className="text-xl md:text-[20px] text-[#161616] leading-[40px] font-normal">
               {t('description-1')}
             </p>
+            <br />
+            <p className="text-[30px] text-[#000000] leading-[40px] font-normal">
+              {t('method-title')}
+            </p>
+            <br />
+            <ul className="ml-6">
+              <li> {t('method.method-1')}</li>
+              <li> {t('method.method-2')}</li>
+            </ul>
+            <br />
+            <p className="text-[30px] text-[#000000] leading-[40px] font-normal">
+              {t('method-title-1')}
+            </p>
+            <br />
+            <ul className="ml-6">
+              <li> {t('method-1.method-1')}</li>
+            </ul>
+            <br />
+            <p className="text-[30px] text-[#000000] leading-[40px] font-normal">
+              {t('method-title-2')}
+            </p>
+            <br />
+            <ul className="ml-6">
+              <li> {t('method-2.method-1')}</li>
+            </ul>
+            <br />
+            <p className="text-[30px] text-[#000000] leading-[40px] font-normal">
+              {t('title-2')}
+            </p>
+            <br />
+            {t('description-2')}
+            <br />
+            {t('description-3')}
           </div>
         </div>
       </Container>
