ac27fe7aa3d891b8c1cb21d42cf311fc3b895ede
Andrii <A.I.Khmelovsky@gmail.com>
2022-09-12 11:27:32 +0300

    refund traanlataion -english

--- site/pages/refund.tsx
+++ site/pages/refund.tsx
@@ -9,10 +9,12 @@ export default function Refund() {
   return (
     <div className="mt-10">
       <Container>
-        <div className="grid grid-cols-1 gap-x-5 mb-10">
+        <div className="grid grid-cols-1 gap-x-5">
           <div className="order-last flex flex-col self-center text-xl md:text-[20px]">
             <p className="text-[30px] text-[#000000] leading-[40px] font-normal">
               {t('title-1')}
+              <br />
+              <br />
             </p>
           </div>
         </div>
@@ -20,6 +22,35 @@ export default function Refund() {
           <div className="order-last flex flex-col self-center text-xl md:text-[20px]">
             <p className="text-xl md:text-[20px] text-[#161616] leading-[40px] font-normal">
               {t('description-1')}
+              <br />
+              <br />
+
+              <p className="text-[30px] text-[#000000] leading-[40px] font-normal">
+                {t('title-2')}
+              </p>
+              <br />
+
+              <ol className="list-decimal">
+                <li className="mb-6"> {t('rules.rule-1')}</li>
+                <li className="mb-6"> {t('rules.rule-2')}</li>
+                <li className="mb-6"> {t('rules.rule-3')}</li>
+                <li className="mb-6"> {t('rules.rule-4')}</li>
+              </ol>
+              {t('decription-3')}
+              <br />
+              <br />
+              <p className="text-[30px] text-[#000000] leading-[40px] font-normal">
+                {t('title-3')}
+              </p>
+              <br />
+              {t('description-4')}
+              <br />
+              <br />
+              <p className="text-[30px] text-[#000000] leading-[40px] font-normal">
+                {t('title-4')}
+              </p>
+              <br />
+              {t('description-5')}
             </p>
           </div>
         </div>
